5547475c7e0146b2d5b70f052babdda413be724b
MarceloNegromonte <mjncavalcante@gmail.com>
2021-10-29 15:34:32 -0300

    post, put, delete, blog pessoal

--- springBoot/ProjetoBlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ springBoot/ProjetoBlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +29,32 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	//assim que for feita alguma requisição do tipo get em postagem/o valor id, sera acessado esse metodo, onde o metodo captura a variavel recebendo dentro do pathvariable e retorna a interface injetada com o @Autowired, o metodo findById devolve tanto um objeto do tipo postagem com ok e o objeto dentro do corpo da requisição, caso aja erro na requição retorna um notfound 
+	@GetMapping("/{id}") //para busca por id, primeiro determinar o metodo, nesse caso get,e coloca qual o parametro que sera enviado para quem ta fazendo a requisição
+	public ResponseEntity<Postagem> GetById(@PathVariable Long id){ //metodo public, tipo response entity, retornando 1 objeto do tipo postagem, nome opcional do metodo, GETBYID
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	//para find(busca)pelo titulo
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){ //requesbody pra pegar o que vem no corpo da requisição
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping //
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){ //requesbody pra pegar o que vem no corpo da requisição
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
 }
--- springBoot/ProjetoBlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ springBoot/ProjetoBlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -34,10 +34,10 @@ public class Postagem {
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 	
 	//Get e Setter
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getTitulo() {
